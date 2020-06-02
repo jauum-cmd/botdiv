6a889c33a385d7a4b9e138767aaa4b1e6638434d
Glitch (hello-express) <none>
2020-06-02 17:29:00 +0000

    🚓👕 Checkpoint ./index.js:8047381/6578

--- index.js
+++ index.js
@@ -8,8 +8,63 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT); // Recebe solicitações que o deixa online
 
-const Discord = require("discord.js"); //Conexão com a livraria Discord.js
-const client = new Discord.Client(); //Criação de um novo Client
-const config = require("./config.json"); //Pegando o prefixo do bot para respostas de comandos
+console.log("♨️ Ligando bot...")
+const Discord = require('discord.js');
+const client = new Discord.Client({autoReconnect: true});
+const config = require("./config.json")
+const { Client, Util } = require('discord.js');
+let prefix = config.prefix
+
+client.on("message", (message) => {
+
+    if (message.channel.type == "dm") return;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+
+    let args = message.content.split(" ").slice(1);
+
+    try {
+        let commandFile = require(`./comandos/${command}.js`);
+        commandFile.run(client, message, args);   
+} catch (err) {
+  console.error(err.stack);
+}
+});
+
+client.on("message", async message => {
+  const regex = /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|club)|discordapp\.com\/invite|discord\.com\/invite)\/.+[a-z]/gi;
+//RegEx com as expressões que normalmente tem na maioria dos links de convites e suas variantes.
+  if (regex.exec(message.content)) {
+    await message.delete({timeout: 1000});
+//Se o conteúdo da mensagem for um convite, o bot apagará a mensagem após um segundo.
+      await message.channel.send(
+        `${message.author} **você não pode postar link de outros servidores aqui!**`
+      );
+//Envia um aviso que ele não pode postar convites naquele chat.
+  }
+});
+
+
+
+
+
+client.on("ready", () => {
+  let activities = [
+      `Utilize ${config.prefix}help para obter ajuda`,
+      `${client.guilds.cache.size} servidores!`,
+      `${client.channels.cache.size} canais!`,
+      `${client.users.cache.size} usuários!`
+    ],
+    i = 0;
+  setInterval( () => client.user.setActivity(`${activities[i++ % activities.length]}`, {
+        type: "WATCHING"
+      }), 1000 * 60);  // WATCHING, LISTENING, PLAYING, STREAMING
+
+  client.user.setStatus("dnd").catch(e => console.error(e.stack));
+console.log("Estou Online!")
+});
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
