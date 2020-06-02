5425e7c22971075742098d12d89112911168d45a
Glitch (hello-express) <none>
2020-06-02 17:41:00 +0000

    🍄🌲 Checkpoint ./package.json:8047381/866 ./comandos/4k.js:8047381/1004 ./comandos/Ajuda.js:8047381/8580 ./comandos/allunban.js:8047381/1147 ./comandos/anal.js:8047381/1006 ./comandos/ass.js:8047381/1005 ./comandos/aviso.js:8047381/1346 ./comandos/avisoembed.js:8047381/1537 ./comandos/bug.js:8047381/1103

---
+++ comandos/bug.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+module.exports.run = (client, message, args) => {
+
+   let icon = message.guild.iconURL || "https://loritta.website/assets/img/unknown.png"
+    let report = args.join(" ");
+    if (!report) return message.reply("insira seu bug.")
+
+    let embed = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setThumbnail(icon)
+        .addField("» Novo bug encontrado:", `** **`)
+        .addField("» Autor(a):", `${message.author.id}`)
+        .addField("» Bug:", `${report}`)
+        .addField("» Servidor:", `${message.guild.name}`)
+        .setTitle(`${message.author.tag} | Bug reportado`, message.author.avatarURL)        
+        .setTimestamp(new Date())
+
+    let canal = client.channels.get('635212350013702145')
+
+    message.delete();
+    canal.send(embed).then(msg => 
+    msg.react("614399503780413480").then(r => 
+    msg.react("613557508577165325")));
+    message.reply(`Obrigado por reportar o bug, meu criador irá conferir e arrumar, quando arrumado te avisaremos!`);
+}
+
+module.exports.help = {
+    name: "bug"
+}
